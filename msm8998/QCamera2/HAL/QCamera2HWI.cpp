bd810caf015f0cbccadef519b1f2b933d196ff33
Michael Bestas <mkbestas@lineageos.org>
2024-03-29 22:49:11 +0200

    msm8998: use Strings8/16 empty()
    Change-Id: I13e4e41813a7a71c2928a680e32c483469aa0c12

--- msm8998/QCamera2/HAL/QCamera2HWI.cpp
+++ msm8998/QCamera2/HAL/QCamera2HWI.cpp
@@ -4590,7 +4590,7 @@ int32_t QCamera2HardwareInterface::configureHDRBracketing()
             tmp.append(",");
     }
 
-    if( !tmp.isEmpty() &&
+    if( !tmp.empty() &&
         ( MAX_EXP_BRACKETING_LENGTH > tmp.length() ) ) {
         //Trim last comma
         memset(aeBracket.values, '\0', MAX_EXP_BRACKETING_LENGTH);
